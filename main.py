949b86797bd9884ab8520a928221fef6b8eddb4f
Aghassi <aghassi@1it.click>
2024-06-19 22:51:57 +0400

    Initial commit

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap5
@@ -18,7 +19,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from gravatar_new import Gravatar
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6d45xzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -59,7 +60,7 @@ class Base(DeclarativeBase):
     pass
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
